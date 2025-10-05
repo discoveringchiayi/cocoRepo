48c146de5ea6aa7e289af8192f396f24806b0881
Axolotl5517 <discoveringchiayi@gmail.com>
2025-10-05 16:05:20 +0800

    Add virtual tour

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0448315649488276,
+        "pitch": 0.10603108354444934,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.010518743370856,
+          "pitch": 0.30367243112606523,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.7548743778043967,
+        "pitch": 0.07639651399657765,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2970069357193772,
+          "pitch": 0.0577796423211403,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -2.778382367811469,
+          "pitch": 0.10975900395283844,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8123513714110437,
+        "pitch": 0.1994396569627117,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6431500576313613,
+          "pitch": 0.09547730784363218,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9901308351774141,
+          "pitch": -0.01454586917185452,
+          "title": "可可樹",
+          "text": "&lt;img src=\\\"img/sample.jpg\\\" width=\\\"250\\\" /&gt;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
